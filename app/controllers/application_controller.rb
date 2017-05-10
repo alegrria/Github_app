f61e85568580444d5be2e1962fed46833b19e0d8
Dende <alegrria@gmail.com>
2017-05-10 11:22:53 +0200

    Make search controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def search
-    render html: "here's a search form"
+  	if params[:search]
+      @repo = Repo.search(params[:search]).order("created_at DESC")
+    else
+      @repo = Repo.paginate(:page => :params[page], :per_page => 10)
+  	end
+  	respond_to do |format|
+      format.html
+  	end
   end
 end
