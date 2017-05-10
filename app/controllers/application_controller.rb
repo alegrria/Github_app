6655a9924fafa6e1026a86e4f5de3e6a16b8be1c
Dende <alegrria@gmail.com>
2017-05-10 12:04:49 +0200

    Edit models

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  def hello
-  	render html: "hello, world!"
-  end
-  def search
-  	if params[:search]
-      @repo = Repo.search(params[:search]).order("created_at DESC")
-    else
-      @repo = Repo.paginate(:page => :params[page], :per_page => 10)
-  	end
-  	respond_to do |format|
-      format.html
-  	end
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
- root 'application#hello'
+ root 'repo#search'
 end
