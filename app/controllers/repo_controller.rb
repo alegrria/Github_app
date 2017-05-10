dbf81b199ab30b20174084e5d28820fb5f292f94
Dende <alegrria@gmail.com>
2017-05-10 22:19:18 +0200

    Add pagination

--- app/controllers/repo_controller.rb
+++ app/controllers/repo_controller.rb
@@ -1,10 +1,12 @@
+require 'will_paginate/array'
+
 class RepoController < ApplicationController
 
   def index
   end
 
   def search
-    @repos = HTTParty.get("https://api.github.com/users/#{params[:name]}/repos?type=owner").parsed_response
+    @repos = HTTParty.get("https://api.github.com/users/#{params[:name]}/repos?type=owner").parsed_response.paginate(page: params[:page], per_page: 5)
     render :index
   end
 end
