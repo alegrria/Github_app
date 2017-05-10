732a370071fe8163cff68bbb24b25280ba6bdc25
Dende <alegrria@gmail.com>
2017-05-10 11:49:48 +0200

    Make repo and user models

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  def hello
+  	render html: "hello, world!"
+  end
   def search
   	if params[:search]
       @repo = Repo.search(params[:search]).order("created_at DESC")
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
- root 'application#search'
+ root 'application#hello'
 end
