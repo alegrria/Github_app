a554138aa393f3220cf3dcd8e2819b9842d17307
Dende <alegrria@gmail.com>
2017-05-10 21:50:15 +0200

    Install HTTParty, search route, index.erb with form

--- app/controllers/repo_controller.rb
+++ app/controllers/repo_controller.rb
@@ -1,18 +1,10 @@
 class RepoController < ApplicationController
 
   def index
-    if params[:search]
-      @repo = Repo.search(params[:search])
-    else  
-      @repo = Repo.paginate(:page => params[:page], :per_page => 15).order("created_at DESC")
-    end
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @repo }
-    end
   end
+
   def search
-    render html: "name, description"
+    response = HTTParty.get "https://api.github.com/users/#{params[:name]}/repos?type=owner"
+    render :index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
  root 'repo#index'
+ get "search", controller: 'repo', action:'search'
 end
